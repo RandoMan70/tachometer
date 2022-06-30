eb52d08a0de551982147f7fd759e2ed1a64dd5a9
Sergey Kovalev <KovalevSS@sibedge.com>
2022-06-30 22:59:55 +0700

    Testing gauge

--- tachometer.ino
+++ tachometer.ino
@@ -2,7 +2,12 @@
 #include "rpmcounter.h"
 #include "trigger.h"
 
-const int NUMLEDS = 60;
+const int LOW_BORDER = 3500;
+const int SWITCH_BORDER = 6800;
+const int HIGH_BORDER = 7200;
+
+
+const int NUMLEDS = 64;
 const int STRIP_PIN = 7;
 const int MAX_RECENT=16;
 const byte IGNITION_PIN = 4;
@@ -13,14 +18,27 @@ microLED< NUMLEDS, STRIP_PIN, MLED_NO_CLOCK, LED_WS2812, ORDER_GRB, CLI_LOW> str
 RPMCounter<9, 2, 7500, 200000, 2> counter;
 Trigger trigger(TRIGGER_FALLING, 200, ignition, NULL, 1000000, timeout, NULL);
 
-void draw() {
-  uint32_t rpm = counter.rpm();
-  strip.leds[0] = mRGB(255, 0, 0);
-  strip.leds[1] = mRGB(0, 255, 0);
-  strip.leds[2] = mRGB(0, 0, 255);
-  strip.leds[19] = mRGB(255, 255, 255);
+void draw(uint32_t rpm) {
+  int bar = rpm * (uint32_t) NUMLEDS / (uint32_t) 7500;
+  mData color = mBlue;
+  if (rpm > HIGH_BORDER) {
+    color = mRed;    
+  } else if (rpm > SWITCH_BORDER) {
+    color = mYellow;
+  } else if (rpm > LOW_BORDER) {
+    color = mGreen;
+  }
+
+  for (int i = 0; i < NUMLEDS; i++) {
+    if (i < bar) {
+      strip.leds[i] = color;
+    } else {
+      strip.leds[i] = mBlack;
+    }
+  }
   strip.show();
-  Serial.println(rpm);
+  
+  Serial.println(rpm, bar);
 }
 
 void timeout(void *) {
@@ -29,17 +47,24 @@ void timeout(void *) {
 
 void ignition(uint32_t timestamp, void *) {
   counter.push(timestamp);
+  draw(counter.rpm());
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(IGNITION_PIN, INPUT);
 
-  strip.setBrightness(10);
+  strip.setBrightness(3);
   strip.clear();
   strip.show();
 }
 
 void loop() {
-  trigger.update(digitalRead(IGNITION_PIN), micros());
+//  trigger.update(digitalRead(IGNITION_PIN), micros());
+  for (int i = 0; i <= 7500; i++) {
+    draw(i);
+  }
+  for (int i = 7500; i >= 0; i--) {
+    draw(i);
+  }
 }
